6ba119bab9461cf308ddb7d88952ac5d114c810c
LijKG <kirubelgirma027@gmail.com>
2023-01-11 09:43:20 -0800

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,10 @@
-file { '/tmp/school':
-  content =>'I love Puppet',
+# Creates a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
+  path    => '/tmp/school',
 }
